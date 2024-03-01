4f2a0ea492e424d0eed28e85f040ab1f00d67699
pxysea <pxysea@163.com>
2024-03-02 00:52:43 +0800

    增加 youtube 解析

--- README.md
+++ README.md
@@ -5,21 +5,27 @@
 ```
 npm i -save  "https://github.com/pxysea/hexo-tag-azplayer.git"
 ```
+## 配置
 
 ## 使用示例
-
+B站视频示例
 ```javascript
 {% azplayer  https://www.bilibili.com/video/BV1QS421K7mD/ %}
 ```
-
-![预览](images/1709295445145.png)
-
-[查看演示](https://az-web.site/)
+Youtube 示例
+```javascript
+{% azplayer  https://www.youtube.com/watch?v=xxxxxxxx '标题' %}
+```
+![预览](images/1709311746355.png)
+![](images/1709311927078.png)
+[查看演示](https://az-web.site/2024/03/01/%E6%B5%8B%E8%AF%95/)
 
 ## 功能列表
 
 1. 支持 Markdown B站链接
+2. 增加 Youtube 支持
 
 ## 感谢
 
 修改自 [hexo-bilibili-plus](https://github.com/pk5ls20/hexo-bilibili-plus)
+[如何在网站中嵌入B 站视频引用](https://cloud.tencent.com/developer/article/1858356)
--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-async function processBilibili(link) {
+'use strict';
+
+hexo.config.azplayer = Object.assign({
+    sandbox_enabled: true
+}, hexo.config.azplayer);
+
+const config = hexo.config.azplayer;
+
+function genIframeCode(iframe) {
+    return `<div style="position: relative; width: 100%; height: 0; padding-bottom: 75%;">${iframe} </div>
+    `
+}
+
+async function processBilibili(link,args) {
     const axios = require('axios');
     let bvid = '';
     let cid = 0;
@@ -48,15 +61,36 @@ async function processBilibili(link) {
 
     if (aid && bvid && cid) {
         console.debug(`AZplayer: aid=${aid},cid=${cid} `);
+        let sandbox = '';
+        if (config.sandbox_enabled)
+            sandbox = `sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts"`;
 
-        return `<div style="position: relative; width: 100%; height: 0; padding-bottom: 75%;">
-    <iframe src="//player.bilibili.com/player.html?aid=${aid}&bvid=${bvid}&cid=${cid}&page=1&high_quality=1&danmaku=0" allowfullscreen="allowfullscreen" width="100%" height="100%" scrolling="no" frameborder="0" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts"  style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"></iframe>
-</div>
-    `
+        return genIframeCode(
+            `<iframe src="//player.bilibili.com/player.html?aid=${aid}&bvid=${bvid}&cid=${cid}&page=1&high_quality=1&danmaku=0" allowfullscreen="allowfullscreen" width="100%" height="100%" scrolling="no" frameborder="0" ${sandbox}  style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"></iframe>`
+        )
     } else {
         return link;
     }
+}
 
+//https://www.youtube.com/watch?v=UuraNOlh-Vk
+function processYoutube(link, args) {
+    
+    const match = link.match(/\/watch\?v=([a-zA-Z0-9-_]+)/);
+    let id='';
+    let title = '';
+
+    if (!match) {
+        return link;
+    }
+    id = match[1];
+    
+    if (args.length > 1)
+        title = args[1];
+
+    return genIframeCode(
+        `<iframe width="1276" height="718" src="https://www.youtube.com/embed/${id}" title="${title}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+    );
 }
 
 hexo.extend.tag.register('azplayer', async function (args) {
@@ -64,10 +98,15 @@ hexo.extend.tag.register('azplayer', async function (args) {
 
     //https://www.bilibili.com/video/BV1QS421K7mD/
     if (/^http[s]?:\/\/www.bilibili.com\//.test(link)) {
-        return await processBilibili(link);
-    } else {
+        return await processBilibili(link,args);
+    } else if (/^http[s]?:\/\/[w.]+?youtube.com\//.test(link)) {
+        return processYoutube(link, args);
+    }
+    else {
         // other 
         console.warn(`AZPlayer: Not support ${link}`);
-        return `<a href='${link}'>${link}</a>`;
+        return genIframeCode(
+            `<iframe src="${link}" allowfullscreen="allowfullscreen" width="100%" height="100%" scrolling="no" frameborder="0"  style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"></iframe>`
+        )
     }
 }, { async: true })
