f3b2533e5c0aa606fcd8e53d45aeaeef79ecfd01
pxysea <pxysea@163.com>
2024-03-02 21:53:01 +0800

    重构

--- README.md
+++ README.md
@@ -3,34 +3,41 @@
 [演示站点](https://az-web.site/)
 
 ## 安装
+
 进入 hexo 主目录，然后执行
+
 ```
 npm i -save  "https://github.com/pxysea/hexo-tag-azplayer.git"
 ```
+
 ## 配置
 
 ## 使用示例
+
 B站视频示例
+
 ```javascript
 {% azplayer  https://www.bilibili.com/video/xxxxxx/  %}
 {% azplayer  https://www.bilibili.com/video/xxxxxxx/ w=100% h=100% allow='allow-top-navigation allow-same-origin allow-forms allow-scripts' %}
 ```
 
 Youtube 示例
+
 ```javascript
 {% azplayer  https://www.youtube.com/watch?v=xxxxxxxx '标题' %}
 {% azplayer https://www.youtube.com/watch?v=xxxx title='xxxx' w=1280 h=720 allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' %}
 ```
 
 参数说明:
+
 ```
 title   标题    可选
 w       宽度    可选
 h       高度    可选
 allow           可选
 ```
-![预览](images/1709311746355.png)
-![](images/1709311927078.png)
+
+
 [查看演示](https://az-web.site/2024/03/01/%E6%B5%8B%E8%AF%95/)
 
 ## 功能列表
--- index.js
+++ index.js
@@ -1,126 +1,23 @@
 'use strict';
-const headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-}
-hexo.config.azplayer = Object.assign({
+const { log,parseArgs } = require('./lib/util');
+const { generateBlIFrame } = require('./lib/bl');
+const { generateYoutuIFrame } = require('./lib/youtu');
 
-}, hexo.config.azplayer);
-
-const config = hexo.config.azplayer;
-
-function genIframeCode(iframe) {
-    // console.log('iframe:',iframe)
-    return `<div style="position: relative; width: 100%; height: 0; padding-bottom: 75%;">${iframe} </div>
-    `
-}
-
-/**
- * 
- * @param { } key key name
- * @param {*} args args array
- * @param {*} dft default value
- * @author pxysea@163.com
- */
-function getArg(key, args, dft = '') {
-    for (let t of args) {
-        let regex = new RegExp(`^['" ]{0,}${key}[ ]{0,}=[ ]{0,}([^'$]+)['" ]{0,}$`,);
-
-        let match = t.match(regex);
-        if (match && match.length > 1) {
-            // console.debug('arg :',key,match[1]);
-            return match[1];
-        }
-    }
-    return dft;
-}
-/**
- * Process bilibili url and generate html code
- * @param {string} link bilibili video url
- * @param {Array} args 
- * @returns iframe code
- * @author pxysea@163.com
- */
-async function processBilibili(link, args) {
-    const axios = require('axios');
-    let bvid = '';
-
-    //引用自 https://github.com/1015770492/bilibili-download/blob/master/doc/bilibili-Api%E6%96%87%E6%A1%A3.md
-    const queryWidthBV = (id) => {
-        const query_url = `http://api.bilibili.com/x/web-interface/view?bvid=${id}`
-        // console.log('query:',query_url);
-        return axios.get(query_url, { headers }).then(({ data }) => {
-            return data;
-        });
-    }
-
-    let match = link.match(/\/video\/([a-zA-Z0-9]+)\//);
-    if (match && match.length > 1) {
-        bvid = match[1];
-    }
-    const resp = await queryWidthBV(bvid);
-
-    // console.info('query resp :', resp)
-    if (!resp || resp.code != 0) {
-        // error
-        console.warn('AZPlayer: query bv error ', bvid, resp.message);
-        return link;
-    } else {
-        let { aid, cid } = resp.data;
-        let sandbox = getArg('allow', args, 'allow-top-navigation allow-same-origin allow-forms allow-scripts');
-        let scrolling = getArg("scrolling", args, 'no');
-        let w = getArg('w', args, '100%');
-        let h = getArg('h', args, '100%');
-
-        return genIframeCode(
-            `<iframe src="//player.bilibili.com/player.html?aid=${aid}&bvid=${bvid}&cid=${cid}&page=1&high_quality=1&danmaku=0" allowfullscreen="allowfullscreen" width="${w}" height="${h}" scrolling="${scrolling}" frameborder="0" sandbox="${sandbox}"  style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"></iframe>`
-
-        )
-    }
-}
-
-//url rule
-//https://www.youtube.com/watch?v=UuraNOlh-Vk
-//https://youtu.be/6mTlXvEkuvU
-function processYoutube(link, args) {
-    // console.log('process youtube:',args);
-    const match = link.match(/\/watch\?v=([a-zA-Z0-9-_]+)/);
-    let id = '';
-
-    if (match && match.length > 1) {
-        id = match[1];
-    }
-    if (id === '') {
-        match = link.match(/youtu.be\/([a-zA-Z0-9-_]+)/);
-        if (match && match.length > 1) {
-            id = match[1];
-        }
-    }
-    if (id === '')
-        return link;
-    let title = getArg('title', args, 'az');
-    let w = getArg('w', args, '1280');
-    let h = getArg('h', args, '720');
-    let allow = getArg('allow', args, 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
-
-    return genIframeCode(
-        `<iframe width="${w}" height="${h}" src="https://www.youtube.com/embed/${id}" title="${title}" frameborder="0" allow="${allow}" allowfullscreen></iframe>`
-    );
-}
 
 hexo.extend.tag.register('azplayer', async function (args) {
-    let link = args[0];
+    let link = args[0]; 
+    
+    log.debug('AZPlayer link:' + link);
+    let p_args = parseArgs(args);    
 
-    //https://www.bilibili.com/video/BV1QS421K7mD/
     if (/^http[s]?:\/\/www.bilibili.com\//.test(link)) {
-        return await processBilibili(link, args);
-    } else if (/^http[s]?:\/\/[w.]+?youtube.com\//.test(link)) {
-        return processYoutube(link, args);
+        return await generateBlIFrame(link,p_args);
+    } else if (/^http[s]?:\/\/[w.]{0,}(youtube.com|youtu.be)\//.test(link)) {
+        return generateYoutuIFrame(link, p_args);
     }
     else {
         // other 
-        console.warn(`AZPlayer: Not support ${link}`);
-        return genIframeCode(
-            `<iframe src="${link}" allowfullscreen="allowfullscreen" width="100%" height="100%" scrolling="no" frameborder="0"  style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"></iframe>`
-        )
+        log.warn(`AZPlayer: Not support ${link}`);
+       return link;
     }
 }, { async: true })
